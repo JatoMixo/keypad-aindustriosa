ffc904068cf9cff3a4d78d193bfdc8722e19fbed
JatoMixo <jatomixo@toshiba.localhost>
2023-11-25 12:46:56 +0100

    Moved pressed key detection to a different function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@
 
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc, gpio::IO, Delay};
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc, gpio::IO, Delay, gpio::Output, gpio::Input, soc::gpio::AnyPin};
+use hal::{gpio::PullUp, gpio::PushPull};
 
 #[entry]
 fn main() -> ! {
@@ -28,9 +29,11 @@ fn main() -> ! {
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
-    println!("Hello world!");
 
+    /* =========== PASSWORD ============ */
+    const CORRECT_PASSWORD: &str = "2734A5";
     let mut password: &str = "";
+
     let mut delay = Delay::new(&clocks);
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
 
@@ -51,43 +54,42 @@ fn main() -> ! {
     ];
 
     // Set columns to low
-    column_gpio.iter_mut().for_each(|mut column| {
+    column_gpio.iter_mut().for_each(|column| {
         column.set_low().unwrap();
     });
 
+    loop {
+        println!("{:?}", get_key_pressed(&mut column_gpio, &mut row_gpio));
+    }
+}
+
+fn get_key_pressed(column_gpio: &mut [AnyPin<Output<PushPull>>; 4], row_gpio: &mut [AnyPin<Input<PullUp>>; 4]) -> Option<char> {
 
     /* =========== KEYBOARD =========== */
-    let keyboard = [["1", "2", "3", "A"],
-                    ["4", "5", "6", "B"],
-                    ["7", "8", "9", "C"],
-                    ["*", "0", "#", "D"]];
-
+    const KEYBOARD: [[char; 4]; 4] = [['1', '2', '3', 'A'],
+                                      ['4', '5', '6', 'B'],
+                                      ['7', '8', '9', 'C'],
+                                      ['*', '0', '#', 'D']];
 
     let mut actual_row = 0;
     let mut actual_column = 0;
-    let mut last_key_pressed = "1";
-    let mut last_time_key_pressed = 0;
 
+    for column in column_gpio.iter_mut() {
+        column.set_low().unwrap();
 
-    loop {
-        for mut column in column_gpio.iter_mut() {
-
-            column.set_low().unwrap();
-
-            for mut row in row_gpio.iter_mut() {
-                if row.is_low().unwrap() {
-                    println!("Key pressed: {}", keyboard[actual_row][actual_column]);
-                    last_key_pressed = keyboard[actual_row][actual_column];
-                }
-
-                actual_row += 1;
+        for row in row_gpio.iter_mut() {
+            if row.is_low().unwrap() {
+                column.set_high().unwrap();
+                return Some(KEYBOARD[actual_row][actual_column]);
             }
 
-            actual_row = 0;
-            actual_column += 1;
-            column.set_high().unwrap();
+            actual_row += 1;
         }
 
-        actual_column = 0;
+        actual_row = 0;
+        actual_column += 1;
+        column.set_high().unwrap();
     }
+
+    None
 }
